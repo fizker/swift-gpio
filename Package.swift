649477cc63a289752756f7b858eea218811a7d37
Benjamin Horsleben <benjamin@fizkerinc.dk>
2025-05-03 14:08:16 +0200

    Bumped Swift version to 5.10

--- Package.swift
+++ Package.swift
@@ -1,16 +1,10 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
 	name: "swift-gpio",
-	platforms: [
-		.iOS(.v10),
-		.macOS(.v10_12),
-		.tvOS(.v10),
-		.watchOS(.v3),
-	],
 	products: [
 		.library(
 			name: "SwiftGPIO",
